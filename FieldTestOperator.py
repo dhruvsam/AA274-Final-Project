1ed05a0da6f01e7542cc41447e0432f1e68d93a2
Dhruv Samant <Brando2017>
2018-11-28 01:22:53 -0800

    Final Project Added

---
+++ FieldTestOperator.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+from std_msgs.msg import Bool
+
+
+def Field_callback(data):
+	rospy.loginfo("Received the following data: ")
+	rospy.loginfo(data.data)
+
+	if(data.data):
+		rospy.loginfo('True received on /ready_to_rescue, sending a True on /rescue_on topic:')
+		pub.publish(True)
+	else:
+		rospy.loginfo('Wrong value received on /ready_to_rescue, sending a False on /rescue_on topic:')
+		pub.publish(False)	
+
+
+def FieldOperator(): 
+	rospy.init_node('FieldTestNode', anonymous=True)
+	pub = rospy.Publisher('/rescue_on', Bool, queue_size = 10)
+	rospy.Subscriber('/ready_to_rescue', Bool, Field_callback)
+	rospy.loginfo("Field tester node initialized ")	
+	rospy.spin()
+
+	# rate = rospy.Rate(1)
+	# while not rospy.is_shutdown():
+	# 	## publishs a string
+	# 	#msg = "hello"
+	# 	msg = True
+	# 	pub.publish(msg)
+	# 	rate.sleep()
+
+
+if __name__ == '__main__':
+    FieldOperator()    
+
